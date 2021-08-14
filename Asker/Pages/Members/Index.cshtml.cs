fce8288e90727b332e66d0ab690568d5fad8f927
Adnan Šupić <adnan.supic@infinitymesh.com>
2021-08-14 17:57:05 +0200

    Members search

--- Asker/Pages/Members/Index.cshtml.cs
+++ Asker/Pages/Members/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Asker.Data;
 using Asker.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Asker.Pages.Members
 {
@@ -21,9 +22,34 @@ namespace Asker.Pages.Members
 
         public IList<Member> Member { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        //public SelectList FullNames { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public string MemberFullName { get; set; }
+
         public async Task OnGetAsync()
         {
-            Member = await _context.Member.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> nameQuery = from m in _context.Member
+                                            orderby m.FullName
+                                            select m.FullName;
+
+            var members = from m in _context.Member
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                members = members.Where(s => s.FullName.Contains(SearchString));
+            }
+
+            //if (!string.IsNullOrEmpty(MemberFullName))
+            //{
+            //    members = members.Where(x => x.FullName == MemberFullName);
+            //}
+            //FullNames = new SelectList(await nameQuery.Distinct().ToListAsync());
+            Member = await members.ToListAsync();
         }
     }
 }
