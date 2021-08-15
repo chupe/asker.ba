4796051ee4d8ed15ec56940a54dd8dfdb7c4496f
Adnan Šupić <adnan.supic@infinitymesh.com>
2021-08-15 09:46:51 +0200

    fees bug sorted

--- Asker/Models/Seed/Data/ItemSeed.cs
+++ Asker/Models/Seed/Data/ItemSeed.cs
@@ -13,8 +13,8 @@ namespace Asker.Models.Seed.Data
                 Name = "Suunto MS2G",
                 Description = "Kompas",
                 Comment = "Za snalazenje u prirodi",
-                Owner = MemberSeed.Entries[0],
-                Lender = MemberSeed.Entries[1],
+                OwnerId = MemberSeed.Entries[0].Id,
+                LenderId = MemberSeed.Entries[1].Id,
                 Amount = 1,
                 Value = 100.5,
                 IsTeamProperty = false
@@ -24,7 +24,7 @@ namespace Asker.Models.Seed.Data
                 Name = "Pentagon MSRP",
                 Description = "Rusak, 65l",
                 Comment = "U dobrom stanju",
-                Lender = MemberSeed.Entries[2],
+                LenderId = MemberSeed.Entries[2].Id,
                 Amount = 2,
                 Value = 399.99,
                 IsTeamProperty = true
@@ -34,8 +34,8 @@ namespace Asker.Models.Seed.Data
                 Name = "WAS Recon plate carrier",
                 Description = "Prsluk",
                 Comment = "youtube.com",
-                Owner = MemberSeed.Entries[3],
-                Lender = MemberSeed.Entries[4],
+                OwnerId = MemberSeed.Entries[3].Id,
+                LenderId = MemberSeed.Entries[4].Id,
                 Amount = 1,
                 Value = 600.999,
                 IsTeamProperty = false
