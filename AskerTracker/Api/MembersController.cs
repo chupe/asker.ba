2f3b69fdfed2f741725e22824bc7d96ed357f2c0
chupe <chupe_@outlook.com>
2021-12-26 19:14:51 +0100

    add members api

---
+++ AskerTracker/Api/MembersController.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AskerTracker.Core;
+using AskerTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskerTracker.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly AskerTrackerDbContext _context;
+
+        public MembersController(AskerTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Members
+        [HttpGet]
+        public IEnumerable<Member> GetMembers()
+        {
+            return _context.Member;
+        }
+
+        // GET: api/Members/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMember([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var member = await _context.Member.FindAsync(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(member);
+        }
+
+        // PUT: api/Members/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMember([FromRoute] Guid? id, [FromBody] Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != member.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(member).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MemberExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Members
+        [HttpPost]
+        public async Task<IActionResult> PostMember([FromBody] Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Member.Add(member);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMember", new {id = member.Id}, member);
+        }
+
+        // DELETE: api/Members/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMember([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var member = await _context.Member.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            _context.Member.Remove(member);
+            await _context.SaveChangesAsync();
+
+            return Ok(member);
+        }
+
+        private bool MemberExists(Guid? id)
+        {
+            return _context.Member.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ AskerTracker/Pages/Members/ClientMembers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AskerTracker.Pages.Members
+{
+    public class ClientMembers : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
