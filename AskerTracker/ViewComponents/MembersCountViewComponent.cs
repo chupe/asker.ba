8f0dc141498178e8883888f76be285344f304cf5
chupe <chupe_@outlook.com>
2021-12-26 01:55:57 +0100

    implement view component

---
+++ AskerTracker/ViewComponents/MembersCountViewComponent.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AskerTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskerTracker.ViewComponents
+{
+    public class MembersCountViewComponent : ViewComponent
+    {
+        private readonly AskerTrackerDbContext _dbContext;
+
+        public MembersCountViewComponent(AskerTrackerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string title)
+        {
+            var count = await _dbContext.Member.CountAsync();
+            
+            return View(count + title.Length);
+        }
+    }
+}
