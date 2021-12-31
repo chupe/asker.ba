6aeafbf2fbeeccc05528ea7c0c88e2a712364fb5
chupe <chupe_@outlook.com>
2021-12-31 19:18:04 +0100

    create no track db context

---
+++ AskerTracker.Infrastructure/AskerTrackerDbContextNoTracking.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AskerTracker.Infrastructure
+{
+    public class AskerTrackerDbContextNoTracking : AskerTrackerDbContext
+    {
+        public AskerTrackerDbContextNoTracking(DbContextOptions<AskerTrackerDbContext> options)
+            : base(options)
+        {
+            base.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+    }
+}
