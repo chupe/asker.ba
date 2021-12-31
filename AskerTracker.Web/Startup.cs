40485f5310534aeb31744bf1347027af56570c55
chupe <chupe_@outlook.com>
2021-12-31 21:43:30 +0100

    fix build issues

--- AskerTracker.Web/Startup.cs
+++ AskerTracker.Web/Startup.cs
@@ -4,8 +4,6 @@ using System.Text.Json.Serialization;
 using AskerTracker.Common;
 using AskerTracker.Domain;
 using AskerTracker.Infrastructure;
-using AskerTracker.Infrastructure.Interfaces;
-using AskerTracker.Infrastructure.Repositories;
 using AskerTracker.Services.Mail;
 using AskerTracker.Settings;
 using Microsoft.AspNetCore.Builder;
@@ -62,13 +60,6 @@ namespace AskerTracker
                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                     options.JsonSerializerOptions.IgnoreNullValues = true;
                 });
-
-            services.AddTransient<IRepository<Member>, MemberRepository>();
-            services.AddTransient<IRepository<EventLocation>, EventLocationRepository>();
-            services.AddTransient<IRepository<Item>, ItemRepository>();
-            services.AddTransient<IRepository<MembershipFee>, MembershipFeeRepository>();
-            services.AddTransient<IRepository<TestingEvent>, TestingEventRepository>();
-            services.AddTransient<IRepository<Training>, TrainingRepository>();
 
             services.Configure<IdentityOptions>(options =>
             {
