9efbc38458ad904e8665a0fb118c9b23914777db
chupe <chupe_@outlook.com>
2022-01-03 02:37:02 +0100

    move preselected member logic to controller

--- AskerTracker.Web/Areas/Domain/Pages/MembershipFees/Create.cshtml.cs
+++ AskerTracker.Web/Areas/Domain/Pages/MembershipFees/Create.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AskerTracker.Common;
 using AskerTracker.Common.Extensions;
@@ -21,19 +22,26 @@ public class CreateModel : AskerTrackerPageModel
 
     [BindProperty] public MembershipFee MembershipFee { get; set; }
 
-    public IEnumerable<SelectListItem> MembersSelectList => Helper.GetSelectList<Member>(_context, m => m.FullName).Result;
+    public IEnumerable<SelectListItem> MembersSelectList { get; set; }
 
     public string ReturnUrl { get; set; }
     public Member PreselectedMember { get; set; }
 
     public async Task<IActionResult> OnGet()
     {
+        MembersSelectList = Helper.GetSelectList<Member>(_context, m => m.FullName).Result;
         ReturnUrl = Request.Headers["Referer"].ToString();
 
         var queryString = new System.Uri(ReturnUrl).Query;
         var queryDictionary = System.Web.HttpUtility.ParseQueryString(queryString);
 
         PreselectedMember = await _context.Members.FindAsync(queryDictionary["id"].ToGuid());
+        if (PreselectedMember != null)
+            MembersSelectList = new List<SelectListItem>
+            {
+                MembersSelectList.First(m => m.Value == PreselectedMember.Id.ToString())
+            };
+        
         ReturnUrl = ReturnUrl.ToRelativePath();
 
         return Page();
@@ -42,6 +50,7 @@ public class CreateModel : AskerTrackerPageModel
     // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
     public async Task<IActionResult> OnPostAsync(string returnUrl)
     {
+        MembersSelectList = Helper.GetSelectList<Member>(_context, m => m.FullName).Result;
         returnUrl ??= Url.Content("~/");
         
         if (!ModelState.IsValid) return Page();
